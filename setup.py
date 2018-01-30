ca5e7187129c181d20179648be7f9aedd2c9fb2b
rizkisasri <sasri.darmajaya@gmail.com>
2018-01-30 09:54:32 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
-
-# -*- coding: utf-8 -*-
 from setuptools import setup
 
 setup(
-      name          = 'decipy',
-      version       = '1.0.1',
-      packages       = ["decipy"],
+      name = 'decipy',
+      version = '1.0.1',
+      packages = ["decipy"],
       py_modules = ['pro','oop'],
-      author        = 'Rizki Sasri Dwitama',
-      author_email  = 'sasri.darmajaya@gmail.com',
-      url           = 'http://www.rizki.com',
-      description   = 'Decipy Multi Criteria Decision Making Library',
+      author = 'Rizki Sasri Dwitama',
+      author_email = 'sasri.darmajaya@gmail.com',
+      url = 'http://www.rizki.com',
+      description  = 'Decipy Multi Criteria Decision Making Library',
       )
